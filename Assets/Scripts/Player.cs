c336d7cb2fb22389660579e1590b45ebb4312f66
AdidasJjolti <118457300+AdidasJjolti@users.noreply.github.com>
2022-12-15 23:11:39 +0900

    플레이어 이동 구현 마무리
    - 이동 방향으로 바라보기
    - 멈춘 상태에서 이전 이동 방향으로 유지
    - Idle, Walk 애니메이션 적용

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,19 +7,47 @@ public class Player : MonoBehaviour
     CharacterController characterController;
     Vector3 moveDirection;
     public int playerSpeed;
+    Animator animator;
 
     void Awake()
     {
         characterController = GetComponent<CharacterController>();
+        animator = GetComponentInChildren<Animator>();
      }
 
-    // Update is called once per frame
     void Update()
     {
         float x = Input.GetAxisRaw("Horizontal");
         float z = Input.GetAxisRaw("Vertical");
         moveDirection = new Vector3(x, 0, z);
 
+        if (x != 0)
+        {
+            moveDirection = new Vector3(x, 0, 0);
+            //isMoving = true;
+            //animator.SetBool("isMoving",true);
+        }
+        else if (z != 0)
+        {
+            moveDirection = new Vector3(0, 0, z);
+            //isMoving = true;
+            //animator.SetBool("isMoving", true);
+        }
+
+
         characterController.Move(moveDirection * playerSpeed * Time.deltaTime);
+
+        moveDirection.y = 0;
+
+        if(moveDirection != Vector3.zero)
+        {
+            transform.rotation = Quaternion.LookRotation(moveDirection);
+        }
+        else
+        {
+            transform.rotation = transform.rotation;
+        }
+
+        animator.SetBool("isMoving", moveDirection != Vector3.zero);
     }
 }
